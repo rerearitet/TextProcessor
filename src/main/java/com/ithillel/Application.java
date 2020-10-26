39190da001870089f03368c03a42a622fe4d1e96
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-10-26 13:20:56 +0200

    Add files via upload

---
+++ src/main/java/com/ithillel/Application.java
@@ -0,0 +1,34 @@
+package com.ithillel;
+
+import com.ithillel.service.ApplicationContext;
+import com.ithillel.service.InMemoryTextProcessor;
+import com.ithillel.service.PropertiesApplicationContext;
+import com.ithillel.service.TextProcessor;
+
+import java.util.Objects;
+
+public class Application {
+
+
+    private ApplicationContext applicationContext = new PropertiesApplicationContext();
+    private TextProcessor textProcessor;
+
+    public Application() {
+        textProcessor = (TextProcessor) applicationContext.getBean("textProcessor");
+    }
+
+    public void save(String key, final String text) {
+        textProcessor.save(key, text);
+    }
+
+    public String getByKey(String key) {
+       return textProcessor.getByKey(key);
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.save("1.txt", "simple text");
+        System.out.println(application.getByKey("1.txt"));
+    }
+
+}
